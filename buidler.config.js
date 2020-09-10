d0e004d7292cbf1fb12cb9c920a8277759af0a4c
PWho <patrick@patrickwho.me>
2020-09-10 12:11:47 -0400

    Update solc version and Buidler config
    I want to use Buidler tooling to get easier access
    to console.log and stack traces, etc. I have tried
    console.log, which works nicely, but I have yet
    to explore the other debugging tools offered.

--- buidler.config.js
+++ buidler.config.js
@@ -1,1 +1,11 @@
-module.exports = {};
+usePlugin("@nomiclabs/buidler-truffle5")
+
+module.exports = {
+    solc: {
+        version: "0.6.2",
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    },
+};
