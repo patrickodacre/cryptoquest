64aa443143f6475efbdf413c7220b94f3a30c62b
PWho <patrick@patrickwho.me>
2020-09-24 15:02:27 -0400

    Use env.js file to store account keys

--- webapp/src/start.js
+++ webapp/src/start.js
@@ -2,14 +2,15 @@ import Web3 from 'web3'
 import zoneABI from '../../build/contracts/ZoneFactory.json'
 import mobABI from '../../build/contracts/MobFactory.json'
 import charABI from '../../build/contracts/CharacterFactory.json'
+import env from '../env.js'
 
 export default async () => {
     console.log(zoneABI)
     const web3js = new Web3(Web3.currentProvider || "ws://127.0.0.1:7545");
     // Now you can start your app & access web3 freely:
-    const characterContractAddress = "0xbbe2c311122dd6BD2EC0eAA284d33D66FbE53980"
-    const mobContractAddress = "0x5aADF92D944c79Eb1d7e2BF3d1A5AA862D6f28E4"
-    const zoneContractAddress = "0x619fA1a899E741Ace8937247Ab88D244E409015A"
+    const characterContractAddress = env.addresses.characters
+    const mobContractAddress = env.addresses.mobs
+    const zoneContractAddress = env.addresses.zones
 
     const zones = new web3js.eth.Contract(zoneABI.abi, zoneContractAddress);
     const characters = new web3js.eth.Contract(charABI.abi, characterContractAddress);
